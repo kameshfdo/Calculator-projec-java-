35572ce46cbf4a2a7b0ec0d727eb8b3ebbafde92
kameshfernando <92473049+kameshfernando@users.noreply.github.com>
2022-12-11 10:29:20 +0530

    Add files via upload

---
+++ calculator/src/Calculator.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Calculator {
+    private JPanel panel1;
+    private JButton subButton;
+    private JButton sumButton;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+    private JButton subButton2;
+
+}
